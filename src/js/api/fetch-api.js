e43cec772bb7ee46d44dbb3369ce074ce02bf339
h0wter <howter99@gmail.com>
2022-05-05 21:59:35 +0300

    Update fetch-api.js

--- src/js/api/fetch-api.js
+++ src/js/api/fetch-api.js
@@ -1,53 +1,6 @@
-const BASE_URL = 'https://api.themoviedb.org/3';
-const API_KEY = 'api_key=e900ddd99edc3affd146f1905e638fd1';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
-const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+import axios from 'axios';
 
-const main = document.getElementById('main');
-const form =  document.getElementById('form');
-const search = document.getElementById('search');
-
-getMovies(API_URL);
-
-function getMovies(url) {
-    fetch(url).then(res => res.json()).then(data => {
-        showMovies(data.results);
-    })
+export default async function getMovies(url) {
+  const result = await axios.get(url);
+  return result.data;
 }
-
-function showMovies(data) {
-    main.innerHTML = '';
-
-    data.forEach(movie => {
-        const {title, poster_path, vote_average, id} = movie;
-        const movieEl = document.createElement('div');
-        movieEl.classList.add('movie');
-        movieEl.innerHTML = `
-             <img class="gallery__img" src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
-            <div class="gallery__thumb">
-                <h3 class="gallery__name">${title}</h3>
-                <p "gallery__description">
-                  <span>${vote_average}</span>
-                </p>
-
-            </div>`
-
-        main.appendChild(movieEl);
-
-        document.getElementById(id).addEventListener('click', () => {
-          console.log(id)
-          openNav(movie)
-        })
-    })
-}
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const searchTerm = search.value;
-    if(searchTerm) {
-        getMovies(searchURL+'&query='+searchTerm)
-    }
-
-})
