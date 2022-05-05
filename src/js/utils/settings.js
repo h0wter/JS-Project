74a3705a7cf54d48ca37837add141a2b8fc430ea
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-05-05 12:37:53 +0300

    adds base_url and api_key

---
+++ src/js/utils/settings.js
@@ -0,0 +1,8 @@
+// Здесь ссылка на фильмотеку, АПИ_КЕЙ 
+
+const settings = {
+    BASE_URL : 'https://api.themoviedb.org/3/',
+    API_KEY : 'e900ddd99edc3affd146f1905e638fd1',
+  }
+  
+  export default settings;
