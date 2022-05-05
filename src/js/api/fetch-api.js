f515423fd60bf013c8277528495e9ffef32c0105
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-05-05 15:35:22 +0300

    adds markup gallery

---
+++ src/js/api/fetch-api.js
@@ -0,0 +1,50 @@
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = 'api_key=e900ddd99edc3affd146f1905e638fd1';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+
+getMovies(API_URL);
+
+function getMovies(url) {
+    fetch(url).then(res => res.json()).then(data => {
+        showMovies(data.results);
+    })
+}
+
+function showMovies(data) {
+    main.innerHTML = '';
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, id} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+             <img class="gallery__img" src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
+            <div class="gallery__thumb">
+                <h3 class="gallery__name">${title}</h3>
+                <p "gallery__description">
+                  <span class="${getColor(vote_average)}">${vote_average}</span>
+                </p>
+
+            </div>`
+
+        main.appendChild(movieEl);
+
+        document.getElementById(id).addEventListener('click', () => {
+          console.log(id)
+          openNav(movie)
+        })
+    })
+}
+
+function getColor(vote) {
+    if(vote>= 8){
+        return 'green'
+    }else if(vote >= 5){
+        return "orange"
+    }else{
+        return 'red'
+    }
+}
+
