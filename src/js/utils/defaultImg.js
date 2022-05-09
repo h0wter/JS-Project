23e818748c7a815e29a9cb35ed4deaf9324f25bf
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-05-09 15:01:30 +0300

    adds function getImgPath

---
+++ src/js/utils/defaultImg.js
@@ -0,0 +1,17 @@
+import IMG_URL from './utils/settings.js';
+import defaultImg from '../../images/default_img/filmoteka.jpg';
+import movieCardTpl from '../movie-card.hbs';
+import { refs } from './refs';
+
+
+export const getImgPath = imgPath => (!imgPath ? `${defaultImg}` : `${IMG_URL}${imgPath}`);
+
+// export default function imagePath(results) {
+//   const normalObjs = results.map(element => {
+//     let temp = normalizationMovieObj(element);
+//     const imgPath = temp.img.value;
+//     imgPath == null || image=='' ? `${defaultImg}` : `${IMG_URL}${imgPath}`;
+
+//   });
+//   refs.galleryList.insertAdjacentHTML('beforeend', movieCardTpl(normalObjs));
+// }
