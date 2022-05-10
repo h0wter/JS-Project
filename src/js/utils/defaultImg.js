70e924511a45448e1cb61a6fe2a5cb3dff932cef
h0wter <howter99@gmail.com>
2022-05-10 15:04:05 +0300

    fixed path

--- src/js/utils/defaultImg.js
+++ src/js/utils/defaultImg.js
@@ -1,8 +1,7 @@
-import IMG_URL from './utils/settings.js';
+import IMG_URL from './settings';
 import defaultImg from '../../images/default_img/filmoteka.jpg';
 import movieCardTpl from '../movie-card.hbs';
 import { refs } from './refs';
-
 
 export const getImgPath = imgPath => (!imgPath ? `${defaultImg}` : `${IMG_URL}${imgPath}`);
 
@@ -16,26 +15,24 @@ export const getImgPath = imgPath => (!imgPath ? `${defaultImg}` : `${IMG_URL}${
 //   refs.galleryList.insertAdjacentHTML('beforeend', movieCardTpl(normalObjs));
 // }
 
-
 export const normalizationMovieObj = ({
-    genre_ids,
-    id,
-    original_title,
-    overview,
-    popularity,
-    poster_path,
-    release_date,
-    vote_average,
-    vote_count,
-  }) => ({
-    genre: genre_ids,
-    id: id,
-    title: original_title,
-    about: overview,
-    popularity: popularity,
-    img: defaultImgPath(poster_path),
-    releaseDate: release_date,
-    vote_average: vote_average,
-    vote_count: vote_count,
-  });
-  
+  genre_ids,
+  id,
+  original_title,
+  overview,
+  popularity,
+  poster_path,
+  release_date,
+  vote_average,
+  vote_count,
+}) => ({
+  genre: genre_ids,
+  id: id,
+  title: original_title,
+  about: overview,
+  popularity: popularity,
+  img: defaultImgPath(poster_path),
+  releaseDate: release_date,
+  vote_average: vote_average,
+  vote_count: vote_count,
+});
