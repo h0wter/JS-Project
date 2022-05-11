bd9ee46372f5b3d5e06b8b198d2cb82e8210eaf3
Kateryna Lytvyn <sam.gilmore@gmail.com>
2022-05-12 01:21:35 +0300

    updated modal
    - to show full genres
    - disabled scroll when modal is opened
    - fixed modal positioning
    - to close modal when clicked outside or ESC

--- src/js/utils/movieModal.js
+++ src/js/utils/movieModal.js
@@ -1,6 +1,8 @@
 import { getMovieById } from './moviesCache';
 import movieModalTpl from '../movie-modal.hbs';
 import getMoviesVideo from '../api/fetch-videos';
+import { getFullGerneNames } from '../getGenreName';
+import refs from './refs';
 
 const movieModalElement = document.querySelector('[data-modal]');
 
@@ -30,15 +32,26 @@ async function showMovieModal(id) {
 
   movieModalElement.innerHTML = markup;
   movieModalElement.classList.remove('is-hidden');
+  refs.body.style.overflow = "hidden";
 
   const modalCloseBtn = document.querySelector('[data-modal-close]');
   modalCloseBtn.addEventListener('click', onClose);
+  refs.backdrop.addEventListener('click', onClose);
+  document.addEventListener('keydown', onClose);
 }
 
 function createMovieModalMarkup(movie) {
+  movie.gerneNamesFull = getFullGerneNames(movie.genre_ids);
   return movieModalTpl(movie);
 }
 
-function onClose() {
-  movieModalElement.classList.add('is-hidden');
+function onClose(event) {
+  if (event.target.classList.contains('backdrop')||event.target.classList.contains('modal__btn--close')||event.keyCode==27) 
+  {
+    movieModalElement.classList.add('is-hidden');
+    refs.body.style.overflow = "auto";
+    document.removeEventListener('keydown', onClose);
+  }
 }
+
+
