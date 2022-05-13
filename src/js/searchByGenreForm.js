a5aae1f8f4efeae852699d3fe70a8f840e124cc1
Mikhail <mihaokryj@gmail.com>
2022-05-14 01:08:53 +0300

    newgenres.1

--- src/js/searchByGenreForm.js
+++ src/js/searchByGenreForm.js
@@ -1,30 +1,3 @@
-<<<<<<< Updated upstream
-import refs from './utils/refs';
-import { getGenre } from './getGenre';
-
-export let genreList;
-
-getGenre()
-  .then(entry => {
-    let searchMarcup = '<option value=0>All</option>'
-    for (let key in entry) {       
-      searchMarcup += "<option value="+key +">"+entry[key] + "</option>"
-            }
-    refs.searcGenreForm.innerHTML = searchMarcup;
-      
-  
-
-    return (genreList = entry);
-  })
-  .catch(error => console.log(error));
-
-
-
-
-
-
-      
-=======
 import refs  from "./utils/refs";
 import NewsApiService from "./api/newsApiService";
 
@@ -45,4 +18,4 @@ apiService.getData()
 
   })
   .catch(error => console.log(error));
->>>>>>> Stashed changes
+
