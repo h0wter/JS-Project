0efe7b14165fb76ef1e732f0e1740b4be6545952
h0wter <howter99@gmail.com>
2022-05-17 19:31:44 +0300

    Update searchGenreForm.js

--- src/js/searchGenreForm.js
+++ src/js/searchGenreForm.js
@@ -1,24 +1,20 @@
-        import refs  from "./utils/refs";
+import refs from './utils/refs';
 
-import NewsApiService from "./api/newsApiService";
+import NewsApiService from './api/newsApiService';
 
 const apiService = new NewsApiService();
 
-                apiService.getGenreURL();
-                apiService
-                .getData().then(entry => {
-                    const genreList =entry.genres;
-                    localStorage.setItem("Genre list", JSON.stringify(genreList))
-                 
-              
-        // const savedSettings = localStorage.getItem("Genre list")
-        // const genreList = JSON.parse(savedSettings)
-       
-            let searchMarcup = '<option value=0>All</option>'
-            for (let i=0; i < genreList.length; i+=1) {       
-              searchMarcup += "<option value="+ genreList[i].id +">"+genreList[i].name + "</option>"
-                    }
-            refs.searcGenreForm.innerHTML = searchMarcup;
-        
-        })
-        
+apiService.getGenreURL();
+apiService.getData().then(entry => {
+  const genreList = entry.genres;
+  localStorage.setItem('Genre list', JSON.stringify(genreList));
+
+  // const savedSettings = localStorage.getItem("Genre list")
+  // const genreList = JSON.parse(savedSettings)
+
+  let searchMarcup = '<option value=0>All Genres</option>';
+  for (let i = 0; i < genreList.length; i += 1) {
+    searchMarcup += '<option value=' + genreList[i].id + '>' + genreList[i].name + '</option>';
+  }
+  refs.searcGenreForm.innerHTML = searchMarcup;
+});
